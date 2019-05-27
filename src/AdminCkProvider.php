e7628e4591dd892bd33e2562add4e74381b26641
fourn <419571893@qq.com>
2019-05-27 15:00:14 +0800

    by fourn

--- src/AdminCkProvider.php
+++ src/AdminCkProvider.php
@@ -3,18 +3,23 @@
 namespace Fourn\AdminCk;
 
 use Illuminate\Support\ServiceProvider;
+use Encore\Admin\Admin;
 use Encore\Admin\Form;
 use Fourn\AdminCk\CkEditor\CkEditor;
 
 class AdminCkProvider extends ServiceProvider
 {
-    public function register()
-    {
-        Form::extend('ckeditor', CkEditor::class);
-    }
-
     public function boot()
     {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/config/ckfinder.php' => config_path('ckfinder.php'),
+//                __DIR__.'/../resources/assets' => public_path('vendor/kindeditor')
+            ]);
+        }
 
+        Admin::booting(function () {
+            Form::extend('ckeditor', CkEditor::class);
+        });
     }
 }
